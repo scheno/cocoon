8f0a69a46d60c430fdf7d4346063d9def51c6ab0
schening <schening@outlook.com>
2021-05-27 00:08:16 +0800

    create customer class

--- refactoring/src/main/java/com/schening/cocoon/refactoring/Customer.java
+++ refactoring/src/main/java/com/schening/cocoon/refactoring/Customer.java
@@ -1,9 +1,46 @@
 package com.schening.cocoon.refactoring;
 
+import java.util.Vector;
+
 /**
  * @author schening
  * @version 1.0
  * @date 2021/5/27 0:00
  */
 public class Customer {
+
+    private String name;
+    private Vector rentals = new Vector();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vector get_rentals() {
+        return rentals;
+    }
+
+    public void set_rentals(Vector rentals) {
+        this.rentals = rentals;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", rentals=" + rentals +
+                '}';
+    }
 }
